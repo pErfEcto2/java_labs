d1b171a35f3699c673ef5369bca39db98f9614e7
pErfEcto2 <misha.yaro2004@gmail.com>
2025-04-26 10:51:29 +0300

    next several tasks completed

--- lab3/task14.java
+++ lab3/task14.java
@@ -7,17 +7,21 @@ import java.util.Comparator;
 public class task14 {
     public static void main(String[] args) {
         Employee[] employees = {
-                new Employee("employee 1", 1),
+                new Employee("employee 3", 2),
+                new Employee("employee 3", 1),
                 new Employee("employee 2", 2),
-                new Employee("employee 3", 3)
+                new Employee("employee 2", 3)
         };
+
+        System.out.println("Before sorting: ");
+        Arrays.stream(employees).forEach(System.out::println);
 
         Arrays.sort(employees, Comparator
                 .comparing(Employee::getSalary)
                 .thenComparing(Employee::getName)
         );
 
-        System.out.println("Сортировка по зарплате и имени:");
+        System.out.println("\nAfter sorting:");
         Arrays.stream(employees).forEach(System.out::println);
 
         Arrays.sort(employees, Comparator
@@ -26,7 +30,7 @@ public class task14 {
                 .reversed()
         );
 
-        System.out.println("\nСортировка в обратном порядке:");
+        System.out.println("\nAfter reverse sorting:");
         Arrays.stream(employees).forEach(System.out::println);
     }
 }
--- lab3/task15.java
+++ lab3/task15.java
@@ -1,7 +1,41 @@
 package lab3;
 
+
+import java.util.Random;
+
+class RandomIntSequence {
+    private static Random generator = new Random();
+
+    private static class RandomSequence implements IntSequence {
+        private int low;
+        private int high;
+
+        public RandomSequence(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
+
+        @Override
+        public int next() {
+            return low + generator.nextInt(high - low + 1);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+    }
+
+    public static IntSequence randomInts(int low, int high) {
+        return new RandomSequence(low, high);
+    }
+}
+
 public class task15 {
     public static void main(String[] args) {
+        IntSequence r = RandomIntSequence.randomInts(1, 10);
 
+        for (int i = 0; i < 5; i++)
+            System.out.println("Next random int is: " + r.next());
     }
 }
